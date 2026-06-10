e7ab53ad0e601426ac505edb78251429c32478e2
Tasneem <tasneem123@gmail.com>
2026-06-11 02:44:01 +0530

    updated

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(
   cors({
 
     origin: [
-    "https://zerodha-dashboard-bsmvs8m5g-tasneemfatma012s-projects.vercel.app/",
+    "https://zerodha-dashboard-bsmvs8m5g-tasneemfatma012s-projects.vercel.app",
     "http://localhost:3000",
     "http://localhost:5173",
     ],
